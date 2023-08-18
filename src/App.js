87418c4560627268b1ca346515b5bda73360d203
nitodeco <98180436+nitodeco@users.noreply.github.com>
2023-08-18 21:20:02 +0200

    Added navigtaion bar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Navigation from './Navigation.js';
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <div className="content">
         <h1>Portfolio</h1>
       </div>
---
+++ src/Navigation.js
@@ -0,0 +1,14 @@
+const Navigation = () => {
+    return (
+        <nav className="navbar">
+            <h1>Nico Möhn</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/projects">Projects</a>
+                <a href="/about">About</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navigation;
