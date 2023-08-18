7e8e0e95b0616c14eaad010704a83e17b8e791d7
nitodeco <98180436+nitodeco@users.noreply.github.com>
2023-08-19 00:16:41 +0200

    Edited homepage

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,8 @@
 const Home = () => {
     return (
         <div className="home">
-            <div id="welcome" className="shadow">
-                <h2>Welcome</h2>
+            <div id="welcome-container">
+                <h2 id="welcome">Welcome</h2>
             </div>
         </div>
     );
