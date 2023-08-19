74b25e5e5665590cd39137188c05c2b602b0a33d
nitodeco <98180436+nitodeco@users.noreply.github.com>
2023-08-19 02:18:12 +0200

    Edited Resume page

--- src/Resume.js
+++ src/Resume.js
@@ -5,9 +5,6 @@ const Resume = () => {
                 <h2 className="title">Resume</h2>
             </div>
             <div className="resume-container">
-                <div class="badge-base LI-profile-badge" data-locale="en_EN" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="nicomoehn" data-version="v1">
-                    <a class="badge-base__link LI-simple-link" href="https://de.linkedin.com/in/nicomoehn?trk=profile-badge" target="_blank">Nico M.</a>
-                </div>
             </div>
         </div>
     );
