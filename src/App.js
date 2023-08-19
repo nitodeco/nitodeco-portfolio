a10d4a9f4b34a66f404c2fb5d42bd8fb1e3c5659
nitodeco <98180436+nitodeco@users.noreply.github.com>
2023-08-19 02:46:25 +0200

    Refacturing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Navigation from './Navigation.js';
 import Home from './Home.js';
