9e0852ed3b4a1adf36a637b8972a39605b370a02
nitodeco <98180436+nitodeco@users.noreply.github.com>
2023-08-19 02:50:46 +0200

    Refacturing

--- src/Navigation.js
+++ src/Navigation.js
@@ -4,9 +4,9 @@ const Navigation = () => {
         <nav className="navbar shadow">
             <h1>Nico Moehn</h1>
             <div className="links">
-                <div className="btn"><a href="/">Home</a></div>
-                <div className="btn"><a href="/projects">Projects</a></div>
-                <div className="btn"><a href="/resume">Resume</a></div>
+                <div className="btn"><a href="/#/">Home</a></div>
+                <div className="btn"><a href="/#/projects">Projects</a></div>
+                <div className="btn"><a href="/#/resume">Resume</a></div>
             </div>
         </nav>
     );
