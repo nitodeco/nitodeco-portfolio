5ac0bcf736903b2e83f623b4c8ff70c60d248b7a
Nico Möhn <nitodeco@Nicos-Macbook.local>
2023-09-23 17:45:32 +0200

    Added utils.js

---
+++ src/utils.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+
+export function useScrollDirection() {
+  const [lastScrollTop, setLastScrollTop] = useState(0);
+  const [direction, setDirection] = useState('up');
+
+  useEffect(() => {
+      function handleScroll() {
+          const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+          if (scrollTop > lastScrollTop) {
+              setDirection('down');
+          } else {
+              setDirection('up');
+          }
+          setLastScrollTop(scrollTop);
+      }
+
+      window.addEventListener('scroll', handleScroll);
+      return () => window.removeEventListener('scroll', handleScroll);
+  }, [lastScrollTop]);
+
+  return direction;
+}
