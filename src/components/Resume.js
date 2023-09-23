da9bb4989a6d7e6c84f6cdcf0023e91b8080ab1d
Nico Möhn <nitodeco@Nicos-Macbook.local>
2023-09-23 17:55:34 +0200

    Updated welcome text

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -34,10 +34,10 @@ const Resume = () => {
                 <p>
                     Hello there! <br/>
                     <br/>
-                    I'm Nico, a {age}-year-old based in Germany who's absolutely passionate of web development. <br /> 
+                    I'm Nico, a {age}-year-old web developer based in Germany. <br /> 
                     This portfolio is my way of sharing the excitement of my journey with you. <br /> 
                     Here, I've put together a collection of demo projects that showcase what I've been working on <br/>
-                    in order to improve my skills.
+                    in order to improve my skills. Enjoy!
                 </p>
             </div>
         </div>
