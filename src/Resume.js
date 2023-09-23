7b834e3d44e1f8b9b129719206dc090259bd0598
Nico Möhn <nitodeco@Nicos-Macbook.local>
2023-09-23 17:07:21 +0200

    Removed home page and moved content into Resume

--- src/Resume.js
+++ src/Resume.js
@@ -1,5 +1,24 @@
 import { useState, useEffect } from 'react';
 
+function calculateAge(birthdate) {
+    const birthYear = birthdate.getFullYear();
+    const birthMonth = birthdate.getMonth();
+    const birthDay = birthdate.getDate();
+  
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    const currentMonth = currentDate.getMonth();
+    const currentDay = currentDate.getDate();
+  
+    let age = currentYear - birthYear;
+  
+    if (currentMonth < birthMonth || (currentMonth === birthMonth && currentDay < birthDay)) {
+      age--;
+    }
+  
+    return age;
+}
+
 function useScrollDirection() {
     const [lastScrollTop, setLastScrollTop] = useState(0);
     const [direction, setDirection] = useState('up');
@@ -23,14 +42,25 @@ function useScrollDirection() {
 }
 
 const Resume = () => {
+    const birthdate = new Date('2002-02-23');
+    const age = calculateAge(birthdate);
+
     const scrollDirection = useScrollDirection();
 
     return (
-        <div className="resume">
+        <div className="home">
             <div className={ `titleContainer ${scrollDirection === 'up' ? 'show' : 'hide'}`}>
-                <h2 className="title">Resume</h2>
+                <h2 className="title">Welcome</h2>
             </div>
-            <div className="resume-container">
+            <div id="intro-container">
+                <p>
+                    Hello there! <br/>
+                    <br/>
+                    I'm Nico, a {age}-year-old based in Germany who's absolutely passionate of web development. <br /> 
+                    This portfolio is my way of sharing the excitement of my journey with you. <br /> 
+                    Here, I've put together a collection of demo projects that showcase what I've been working on <br/>
+                    in order to improve my skills.
+                </p>
             </div>
         </div>
     );
